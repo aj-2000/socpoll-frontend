313125ab66295b48d36f5d00be6eff45b1872945
aj2000 <hi@ajaysharma.dev>
2023-03-09 21:43:59 +0530

    Added: SidebarMenuItem + Code Refactoring

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,6 +1,6 @@
 
 <script lang="ts">
-import Icon from '@iconify/svelte';
+  import SidebarMenuItem from './SidebarMenuItem.svelte';
 </script>
 
 <div class="col-span-1 bg-black text-white h-screen py-6 px-4">
@@ -9,30 +9,12 @@ import Icon from '@iconify/svelte';
             <div class="text-4xl font-bold italic py-6 cursor-pointer">
                 SocPoll
             </div>
-            <div class="flex gap-4 items-center cursor-pointer">
-               <Icon class="text-4xl text"  icon="material-symbols:home" />
-               <span class="text-md font-semibold">Home</span>
-            </div>
-            <div class="flex gap-4 items-center cursor-pointer">
-               <Icon class="text-4xl text"  icon="material-symbols:explore" />
-               <span class="text-md font-semibold">Explore</span>
-            </div>
-            <div class="flex gap-4 items-center cursor-pointer">
-               <Icon class="text-4xl text"  icon="jam:messages-f" />
-               <span class="text-md font-semibold">Messages</span>
-            </div>
-            <div class="flex gap-4 items-center cursor-pointer">
-               <Icon class="text-4xl text"  icon="system-uicons:create" />
-               <span class="text-md font-semibold">Create</span>
-            </div>
-            <div class="flex gap-4 items-center cursor-pointer">
-               <Icon class="text-4xl text"  icon="gg:profile" />
-               <span class="text-md font-semibold">Profile</span>
-            </div>
+            <SidebarMenuItem title="Home" icon="material-symbols:home"/>
+            <SidebarMenuItem title="Explore" icon="material-symbols:explore"/>
+            <SidebarMenuItem title="Messages" icon="jam:messages-f"/>
+            <SidebarMenuItem title="Create" icon="system-uicons:create"/>
+            <SidebarMenuItem title="Profile" icon="gg:profile"/>
          </div>
-        <div class="flex gap-4 items-center cursor-pointer">
-            <Icon class="text-4xl text"  icon="charm:menu-hamburger" />
-            <span class="text-md font-semibold">More</span>
-        </div>
+        <SidebarMenuItem icon="charm:menu-hamburger" title="More"/>
     </div>
 </div>
---
+++ src/components/SidebarMenuItem.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import Icon from '@iconify/svelte';
+    export let icon = ""
+    export let title = ""
+</script>
+
+<div class="flex gap-4 items-center cursor-pointer">
+    <Icon class="text-4xl text"  icon={icon} />
+    <span class="text-md font-semibold">{title}</span>
+</div>
