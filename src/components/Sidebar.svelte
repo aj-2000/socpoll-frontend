ab30c5e77faa4cb064e34c5b4fb05f6ee0d5be0c
aj2000 <hi@ajaysharma.dev>
2023-03-09 21:22:32 +0530

    Added: Sidebar Component + Root Layout

---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,38 @@
+
+<script lang="ts">
+import Icon from '@iconify/svelte';
+</script>
+
+<div class="col-span-1 bg-black text-white h-screen py-6 px-4">
+    <div class="flex flex-col justify-between h-full ">
+        <div class="flex flex-col gap-6 h-full">
+            <div class="text-4xl font-bold italic py-6 cursor-pointer">
+                SocPoll
+            </div>
+            <div class="flex gap-4 items-center cursor-pointer">
+               <Icon class="text-4xl text"  icon="material-symbols:home" />
+               <span class="text-md font-semibold">Home</span>
+            </div>
+            <div class="flex gap-4 items-center cursor-pointer">
+               <Icon class="text-4xl text"  icon="material-symbols:explore" />
+               <span class="text-md font-semibold">Explore</span>
+            </div>
+            <div class="flex gap-4 items-center cursor-pointer">
+               <Icon class="text-4xl text"  icon="jam:messages-f" />
+               <span class="text-md font-semibold">Messages</span>
+            </div>
+            <div class="flex gap-4 items-center cursor-pointer">
+               <Icon class="text-4xl text"  icon="system-uicons:create" />
+               <span class="text-md font-semibold">Create</span>
+            </div>
+            <div class="flex gap-4 items-center cursor-pointer">
+               <Icon class="text-4xl text"  icon="gg:profile" />
+               <span class="text-md font-semibold">Profile</span>
+            </div>
+         </div>
+        <div class="flex gap-4 items-center cursor-pointer">
+            <Icon class="text-4xl text"  icon="charm:menu-hamburger" />
+            <span class="text-md font-semibold">More</span>
+        </div>
+    </div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
   import "../app.css";
+  import Sidebar from "../components/Sidebar.svelte";
 </script>
 
-<slot />
+
+<div class="grid grid-cols-6" >
+  <Sidebar/>
+  <div class="col-span-5 bg-base-300 h-screen">
+    <slot />
+  </div>
+</div>
+
+
