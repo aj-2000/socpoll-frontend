29888206df46eafbc9b75ccf4208bc890a95b807
aj2000 <hi@ajaysharma.dev>
2023-03-10 23:07:04 +0530

    WIP: ADDED: Authentication

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,34 @@
 <script>
   import "../app.css";
+  import { applyAction, enhance } from '$app/forms'
+  import { invalidateAll } from '$app/navigation'
+  import { page } from '$app/stores'
   import Sidebar from "../components/Sidebar.svelte";
 </script>
 
+<nav>
+  {#if !$page.data.user}
+    <a href="/login">Login</a>
+    <a href="/register">Register</a>
+  {/if}
+
+  {#if $page.data.user}
+    <a href="/admin">Admin</a>
+
+<form
+  action="/logout"
+  method="POST"
+  use:enhance={() => {
+    return async ({ result }) => {
+      invalidateAll()
+      await applyAction(result)
+    }
+  }}
+>
+      <button type="submit">Log out</button>
+    </form>
+  {/if}
+</nav>
 
 <div class="grid grid-cols-6" >
   <Sidebar/>
